29105a350db8bfa032d77c4e2ae5b872a240fbac
Bl00D4NGEL <dominik.peters@cura4you.de>
2024-05-28 07:04:27 +0200

    chore: remove unused import

--- src/Command/GenerateKeysCommand.php
+++ src/Command/GenerateKeysCommand.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace GeekCell\SodiumBundle\Command;
 
 use GeekCell\SodiumBundle\Sodium\Crypto\Box;
-use GeekCell\SodiumBundle\Sodium\Sodium;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
